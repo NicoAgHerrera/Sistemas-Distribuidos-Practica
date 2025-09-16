4fcaa35bc9d2bda0f6aca0ea82fe98ab018f1e9a
NicoAgHerrera <132090401+NicoAgHerrera@users.noreply.github.com>
2025-09-16 09:31:59 -0300

    Comentarios en page.tsx

--- mi-proyecto/app/page.tsx
+++ mi-proyecto/app/page.tsx
@@ -1,11 +1,18 @@
-import Titulo from "@/components/Titulo";
-import Parrafo from "@/components/Parrafo";
-import BotonLike from "@/components/BotonLike";
-import Imagen from "@/components/Imagen";
+//Importación de los componentes a utilizar en la página
+//Cada uno definido en la carpeta components
+import Titulo from "@/components/Titulo"; // Componente que muestra un título en pantalla
+import Parrafo from "@/components/Parrafo";   // Componente que muestra texto en párrafos
+import BotonLike from "@/components/BotonLike"; // Botón interactivo que cuenta "likes"
+import Imagen from "@/components/Imagen";  // Componente que muestra una imagen 
 
-export default function Home() {
+
+// Componente principal de la página raíz ("/").
+// Next.js renderiza este componente cuando el usuario visita la ruta "/".
+export default function Home() {// Se exporta la función Home como el componente principal de esta página.
   return (
-    <main className="p-5 font-sans bg-gray-900 text-white min-h-screen">
+    //El contenido de la pagina se estructura en un main con un estilo dado por Tailwind (espaciado, fuente, color, altura mínima, etc.)
+    //Dentro de main se utizan los diferentes componentes que dan vida a la pagina, pasandoles por parametro los props que necesitan
+    <main className="p-5 font-sans bg-gray-900 text-white min-h-screen"> 
       <Titulo texto="Página de prueba" />
       
       <Parrafo>
